d2a87e81f8110483ce99c7285a260306c663837a
HugoVG <64896329+HugoVG@users.noreply.github.com>
2020-09-22 16:31:01 +0200

    non-auto version provided

--- GOGOThemes.py
+++ GOGOThemes.py
@@ -27,47 +27,89 @@ else:
 
 
 def apply(Go_Thema):
-    print("going {}".format(Go_Thema))
-    # kijkt of de dir bestaat zoja remove
-    if os.path.exists("./bin/wwwroot/css/"):
-        try:
-            shutil.rmtree("./bin/wwwroot/css/")
-        except:
-            print("Error")
-        print("rm css")
-    if os.path.exists("./bin/wwwroot/themes/"):
-        try:
-            shutil.rmtree("./bin/wwwroot/themes/")
-        except:
-            print("Error")
-        print("rm themes")
-    if os.path.exists("./bin/wwwroot/assets/"):
-        try:
-            shutil.rmtree("./bin/wwwroot/assets/")
-        except:
-            print("Error")
-        print("rm assets")
+    if var1.get() == 1:
+        print("going {}".format(Go_Thema))
+        # kijkt of de dir bestaat zoja remove
+        if os.path.exists("./bin/wwwroot/css/"):
+            try:
+                shutil.rmtree("./bin/wwwroot/css/")
+            except:
+                print("Error")
+            print("rm css")
+        if os.path.exists("./bin/wwwroot/themes/"):
+            try:
+                shutil.rmtree("./bin/wwwroot/themes/")
+            except:
+                print("Error")
+            print("rm themes")
+        if os.path.exists("./bin/wwwroot/assets/"):
+            try:
+                shutil.rmtree("./bin/wwwroot/assets/")
+            except:
+                print("Error")
+            print("rm assets")
 
-    if os.path.exists("./skins/{}/wwwroot".format(Go_Thema)):
-        print("main route")
-        fromdir = "./skins/{}/".format(Go_Thema)
-        todir = "./bin/"
-        copy_tree(fromdir, todir)
-    elif os.path.exists("./skins/{}/resources/app/desktop/student/wwwroot".format(Go_Thema)):
-        print("diffrent route")
-        fromdir = "./skins/{}/resources/app/desktop/student/".format(Go_Thema)
-        todir = "./bin/"
-        copy_tree(fromdir, todir)
+        if os.path.exists("./skins/{}/wwwroot".format(Go_Thema)):
+            print("main route")
+            fromdir = "./skins/{}/".format(Go_Thema)
+            todir = "./bin/"
+            copy_tree(fromdir, todir)
+        elif os.path.exists("./skins/{}/resources/app/desktop/student/wwwroot".format(Go_Thema)):
+            print("diffrent route")
+            fromdir = "./skins/{}/resources/app/desktop/student/".format(Go_Thema)
+            todir = "./bin/"
+            copy_tree(fromdir, todir)
 
-        fromdir = "./themes/"
-        todir = "./bin/wwwroot/"
-        copy_tree(fromdir, todir)
+            fromdir = "./themes/"
+            todir = "./bin/wwwroot/"
+            copy_tree(fromdir, todir)
+        else:
+            print("waar de fk heb je alle bestanden gelaten?")
+            
+        #Kamikaze
+        root.destroy()
     else:
-        print("waar de fk heb je alle bestanden gelaten?")
+        if var1.get() == 0:
+            print("going {}".format(Go_Thema))
+        # kijkt of de dir bestaat zoja remove
+        if os.path.exists("./resources/app/desktop/Student/"):
+            try:
+                shutil.rmtree("/resources/app/desktop/Student/wwwroot/css/")
+            except:
+                print("Error")
+            print("rm css")
+        if os.path.exists("./resources/app/desktop/Student/themes/"):
+            try:
+                shutil.rmtree("./resources/app/desktop/Student/themes/")
+            except:
+                print("Error")
+            print("rm themes")
+        if os.path.exists("./resources/app/desktop/Student/assets/"):
+            try:
+                shutil.rmtree("./resources/app/desktop/Student/assets/")
+            except:
+                print("Error")
+            print("rm assets")
 
-    
-    #Kamikaze
-    root.destroy()
+        if os.path.exists("./skins/{}/wwwroot".format(Go_Thema)):
+            print("main route")
+            fromdir = "./skins/{}/".format(Go_Thema)
+            todir = "./resources/app/desktop/Student"
+            copy_tree(fromdir, todir)
+        elif os.path.exists("./skins/{}/resources/app/desktop/student/wwwroot".format(Go_Thema)):
+            print("diffrent route")
+            fromdir = "./skins/{}/resources/app/desktop/student/".format(Go_Thema)
+            todir = "./resources/app/desktop/Student"
+            copy_tree(fromdir, todir)
+
+            fromdir = "./themes/"
+            todir = "./skins/{}/resources/app/desktop/student/wwwroot/"
+            copy_tree(fromdir, todir)
+
+        #Kamikaze
+        root.destroy()
+
+
 
 
 #innit root en frame voor root
@@ -107,6 +149,9 @@ aLab.place(x=themaW, y=themaH)
 themaH += 30
 aBut = Button(frame, text=str("apply"), command=lambda:apply(allskins[2]))
 aBut.place(x=themaW, y=themaH)
+
+var1 = IntVar()
+Checkbutton(frame, text="Auto?", variable=var1).place(x=300, y=10)
 
 frame.pack()
 #dit voorkomt dat de frame geresized kan worden verkomt lelijkheid(synoniem = ik)
